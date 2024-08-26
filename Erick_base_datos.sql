dc9a3bac9b36a10591633f256ef93c2ee98c3bda
Danieldd96 <danieldelgado.dd270@gmail.com>
2024-08-26 11:31:26 -0600

    Union, creacion del procedures para realizar una reserva junto con cambios a la tabla reservas

--- Erick_base_datos.sql
+++ Erick_base_datos.sql
@@ -1,7 +1,7 @@
 
 CREATE DATABASE Reservas
     DEFAULT CHARACTER SET = 'utf8mb4';
-    USE pagina_hotel;
+    USE proyecto_base_datos;
     -- Crear la tabla Transaccion
 CREATE TABLE Transaccion (
     Numero_transaccion INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -9,20 +9,24 @@ CREATE TABLE Transaccion (
     Metodo_Pago VARCHAR(50) NOT NULL,
     Coste INT NOT NULL
 );
-
 -- Crear la tabla Reservas
 CREATE TABLE Reservas (
     Reserva_id INT PRIMARY KEY AUTO_INCREMENT,
     Llegada DATE NOT NULL,
     Salida DATE NOT NULL,
     Numero_huespedes INT NOT NULL,
-    Numero_transaccion INT,
-    FOREIGN KEY (Numero_transaccion) REFERENCES Transaccion(Numero_transaccion)
+    Numero_transaccion DEFAULT 10,
+    Estado BOOLEAN DEFAULT 0,
+    UsuarioID INT,
+    ID_Habitacion INT,
+    FOREIGN KEY (Numero_transaccion) REFERENCES Transaccion(Numero_transaccion),
+    FOREIGN KEY (UsuarioID) REFERENCES Usuarios(UsuarioID),
+    FOREIGN KEY (ID_Habitacion) REFERENCES Habitaciones(ID_Habitacion)
 );
 
 -- Crear la tabla Habitaciones
 CREATE TABLE Habitaciones (
-    ID_Habiitacion INT PRIMARY KEY AUTO_INCREMENT,
+    ID_Habitacion INT PRIMARY KEY AUTO_INCREMENT,
     Precio DECIMAL(10, 2) NOT NULL,
     Capacidad INT NOT NULL,
     Disponibilidad BOOLEAN NOT NULL
@@ -43,14 +47,8 @@ VALUES
 (90.00, 3, FALSE);
 
 -- Insertar datos en la tabla Reservas
-INSERT INTO Reservas (Llegada, Salida, Numero_huespedes, Numero_transaccion)
+INSERT INTO Reservas (Llegada, Salida, Numero_huespedes,UsuarioID,ID_Habitacion)
 VALUES 
-('2024-08-24', '2024-08-26', 2, 1),
-('2024-08-25', '2024-08-30', 4, 2),
-('2024-08-26', '2024-08-27', 3, 3);
-
-CREATE View [Nuevas_reservas] AS
-SELECT Precio,Capacidad,Disponibilidad FROM Habitaciones
-WHERE Precio = 90.00
-
-SELECT * FROM [Nuevas_reservas];
+('2024-08-24', '2024-08-26', 2,3,1),
+('2024-08-25', '2024-08-30', 4,2,2),
+('2024-08-26', '2024-08-27', 3,1,3);

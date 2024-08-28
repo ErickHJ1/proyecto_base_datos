374743062a69950a81797ac3d486a7f61067556a
Danieldd96 <danieldelgado.dd270@gmail.com>
2024-08-28 09:30:00 -0600

    Creacion del segundo querie de disponibilidad de habitaciones en una fecha dada

--- Queries.sql
+++ Queries.sql
@@ -1,4 +1,10 @@
 ---Querys 
+
+--2
+SELECT  Nombre,COUNT(`Habitaciones`.disponibilidad) AS cant_habitaciones
+FROM Hoteles INNER JOIN Habitaciones ON `Hoteles`.HotelID = `Habitaciones`.ID_Habitacion
+INNER JOIN Reservas ON `Habitaciones`.ID_Habitacion = `Reservas`.ID_Habitacion 
+WHERE Disponibilidad = TRUE AND Reservas.Salida < "2024-12-31" GROUP BY HotelID;
 --3
 SELECT Nombre FROM hoteles ;
 --4
